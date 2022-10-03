db3ef01c3d2eef05e0bc51787a6b437790ed34b2
anchalpandey1 <anchalpandey9076@gmail.com>
2022-10-04 01:27:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './component/Navbar';
+import UseEffectAPI from './component/useEffect/useEffectAPI';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Navbar />
+        <UseEffectAPI/>
+        
+    </>
+      
+    </div>
+  );
+}
+
+export default App;
